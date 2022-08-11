0b1d690b69d73db5f0be090bbf122004fd067b94
Yuki <ghost.in.the.shell.innocence9@gmail.com>
2022-08-11 16:00:59 +0900

    entrance style sp

--- script/script.js
+++ script/script.js
@@ -1,4 +1,14 @@
-$(function(){
+$(function () {
 
+  //ハンバーガーメニューボタン
+  $('.burgerButton').on('click', function () {
+    $('.burgerButton').toggleClass('close');
+    $('.navWrapper').toggleClass('slide-in');
+  });
+
+  $('.navWrapper a').on('click', function () {
+    $('.navWrapper').removeClass('slide-in');
+    $('.burgerButton').toggleClass('close');
+  });
 
 });
